76e9a900cb446d00e79aafa2af2423608acfad25
teto <nestor9623@hotmail.com>
2016-09-28 20:36:36 -0500

    Creacio del proyecto
    creacion del metodo login
    
    tipo:nuevo proyecto

---
+++ examenLogin/src/Modelo/usuario.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author nesto
+ */
+public class usuario {
+    
+    private String usuario;
+    private String password;
+    private String nombre;
+    private String direccion;
+    
+
+    public usuario(){
+        
+    }
+    
+    public usuario(String usuario, String password, String nombre, String direccion) {
+        this.usuario = usuario;
+        this.password = password;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+    
+    
+}
